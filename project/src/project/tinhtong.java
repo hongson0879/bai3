abc10fe9ab0a81bf12f6006738b4260fb87ceca2
arkchesnguyen <arkchesnguyen@users.noreply.github.com>
2020-05-11 22:27:10 +0700

    Thêm Class tính tổng

--- project/src/project/tinhtong.java
+++ project/src/project/tinhtong.java
@@ -1,5 +1,23 @@
 package project;
 
-public class tinhtong {
+import java.util.Scanner;
 
+public class tinhtong {
+		public static void main(String[] args) {
+			int a,b;
+			
+			Scanner sc = new Scanner (System.in);
+			
+			System.out.print("Nhap gia tri a: ");
+			a = sc.nextInt();
+			
+			System.out.print("Nhap gia tri b: ");
+			b = sc.nextInt();
+			
+			int c = a+b;
+			
+			System.out.print("Tong cua "+a+" + "+b+" = "+c);
+			
+					
+		}
 }
